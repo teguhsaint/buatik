fc3a96b1a21286cfae96d4f9bf577852b3d3987d
Teguh Saint <75825300+teguhsaint@users.noreply.github.com>
2023-01-06 21:55:15 +0700

    Create penjualan.php

---
+++ pages/penjualan.php
@@ -0,0 +1,23 @@
+<a href="index.php?p=transaksi" class="btn btn-sm btn-primary my-3 float-end"><i class="fas fa-plus"></i> Tambah Transaksi</a>
+<table class="table table-bordered">
+    <thead>
+        <th>NO</th>
+        <th>Kode Penjualan</th>
+        <th>Tanggal</th>
+        <th>Metode Pembayaran</th>
+        <th>Status</th>
+        <th>Piutang</th>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+
+        </tr>
+    </tbody>
+
+</table>
